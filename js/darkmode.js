5cb7c71c743b1451875b685d91080fb3ace8dc92
kritzy_8 <135436584+kritzy8@users.noreply.github.com>
2024-08-19 14:49:45 +0700

    change light-dark switcher method and make it remember

---
+++ js/darkmode.js
@@ -0,0 +1,35 @@
+/*var switchtheme = document.getElementById("switchtheme");
+
+switchtheme.onclick = function () {
+    document.body.classList.toggle("light-theme");
+};
+*/
+let lightMode = localStorage.getItem('lightMode');
+const lightModeToggle = document.querySelector('#switchtheme');
+
+console.log(lightMode);
+
+const enableLightMode = () => {
+    document.body.classList.add('light-theme');
+    localStorage.setItem('lightMode','enable');
+};
+
+const disableLightMode = () => {
+    document.body.classList.remove('light-theme');
+    localStorage.setItem('lightMode',null);
+};
+
+if (lightMode === 'enable') {
+    enableLightMode();
+}
+
+lightModeToggle.addEventListener('click', () => {
+    lightMode = localStorage.getItem('lightMode');
+    if (lightMode !== 'enable') {
+        enableLightMode();
+        console.log(lightMode);
+    } else {
+        disableLightMode();
+        console.log(lightMode);
+    }
+});
